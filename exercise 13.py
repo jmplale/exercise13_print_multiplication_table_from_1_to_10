6fdaaa1dae8f47b837fce090b823d4e9cd32311c
johnmichaelpplale <johnmichaelpplale@gmail.com>
2024-01-22 18:10:17 +0800

    use a for loop inside a for loop

--- exercise 13.py
+++ exercise 13.py
@@ -2,5 +2,7 @@
 def product(number_one, number_two):
     # use a for loop
     for multiplicand in range(number_one,number_two):
-       
-       
+        # use a for loop inside a for loop
+        for multiplier in range(number_one,number_two):
+            print(multiplicand * multiplier, end=" ")
+            print(" ")
