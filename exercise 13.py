b28c2ba09b631da84bf18331b6ca23a5584e4266
johnmichaelpplale <johnmichaelpplale@gmail.com>
2024-01-22 18:00:54 +0800

    use a for loop

--- exercise 13.py
+++ exercise 13.py
@@ -1,6 +1,6 @@
 # make a def function
 def product(number_one, number_two):
     # use a for loop
-    for multipicland in range(number_one,number_two):
-        
+    for multiplicand in range(number_one,number_two):
+       
        
